ead26f82c928a45c681ed0cd4d4eba576ac01c55
bjorgjes <bjornar.gjesdal@live.com>
2019-03-11 10:54:17 +0100

    setup for remote pc

--- iypoint32.f95
+++ iypoint32.f95
@@ -73,12 +73,12 @@ k = 0
 !Fp0 = 0
 !s0 = 16
 bcond = 2
-part = 1
+part = 5
 
-call OMP_SET_NUM_THREADS(2)
+call OMP_SET_NUM_THREADS(7)
 !$OMP PARALLEL PRIVATE(F0,S0,Fp0,bryter,propconst,bcond,pw1,k,slip,Cel,eul,nlines,id,R, hardening, pi, dt, tag,epsp)
 !$OMP DO
-do k = 0,2
+do k = 0,part
     bryter = 5
     pw1 = 0.001
     bcond = 2
@@ -95,6 +95,29 @@ end do
 !$OMP END DO NOWAIT
 !$OMP END PARALLEL
 
+
+bcond = 2
+part = 5
+
+call OMP_SET_NUM_THREADS(7)
+!$OMP PARALLEL PRIVATE(F0,S0,Fp0,bryter,propconst,bcond,pw1,k,slip,Cel,eul,nlines,id,R, hardening, pi, dt, tag,epsp)
+!$OMP DO
+do k = 0,part
+    bryter = 5
+    pw1 = 0.001
+    bcond = 2
+    write(*,*) 'start'
+    propconst = (/0.0, 0.0, 0.0, 0.0, -0.1*k/)
+    call newton(k,2,bryter,bcond,F0,Fp0,S0,pw1,propconst) 
+    bryter = 5
+    tag = 0
+    epsp = 0
+    call constexpr(k,2,bryter,bcond,pw1, tag, epsp,propconst)
+    write(*,*) tag(1,1), tag(2,2) , k
+    write(*,*) 'cycle'
+end do
+!$OMP END DO NOWAIT
+!$OMP END PARALLEL
 
 
 !bryter = 6
